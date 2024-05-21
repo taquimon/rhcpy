374853f7f6b2666d88cee05e2479bb8faad9d02b
Edwin Taquichiri <edwin.taquichiri@jalasoft.com>
2024-05-20 22:12:46 -0400

    upload botafogo files

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+    
 from flask import Flask, redirect, url_for, render_template, request
 
 from helpers.rhc_helper import read_json_file
@@ -16,16 +18,39 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route("/<usr>")
+@app.route("/<usr>", methods=['POST', 'GET'])
 def user(usr):
     return f"<h1>{usr}</h1>"
 
-@app.route("/player/<club>")
-def club(club):
-    players = read_json_file(club_name=club)
-    print(players[0])
-    return render_template("player.html", players=players)
+@app.route("/club", methods=['POST'])
+def club():
+    club_name = ''    
+    if request.method == "POST":
+        default_value = '0'
+        
+        club_name = request.form.get('club', default_value)
+    print("Club: ", club_name)
+    players = read_json_file(club_name=club_name)    
+    print(players)
+    for player in players:
+        age = calculate_age(player['fechanacimiento'])
+        player["edad"] = age
+        if age >=50:
+            player["bg"] = "bg-primary"
+        else:
+            player["bg"] = "bg-warning"
+        
+    print(players)
+    return render_template("player.html", players=players, club=club_name)
     
+def calculate_age(birthdate):
+
+    
+    day,month,year = map(int, birthdate.split("-"))
+    today = datetime.date.today()
+    age = today.year - year - ((today.month, today.day) < (month, day))
+    
+    return age
 
 if __name__ == "__main__":
     app.run(debug=True)
--- helpers/rhc_helper.py
+++ helpers/rhc_helper.py
@@ -2,7 +2,7 @@ import json
 from config.config import abs_path
 def read_json_file(club_name):
     
-    with open(f"{abs_path}/resources/data.json") as json_file:
+    with open(f"{abs_path}/static/resources/{club_name.lower()}.json") as json_file:
         data = json.load(json_file)
     
     return data
